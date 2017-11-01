8492ed28b41262b6339c51db13174d5c223686f4
ricardopimentel <ricardoppimentel@gmail.com>
2017-11-01 16:54:02 -0300

    Alteração do local do arquivo para pypdfocr

--- restaurante/relatorios/views.py
+++ restaurante/relatorios/views.py
@@ -120,7 +120,7 @@ def PdfVendas(request):
     if platform.system() == 'Windows':
         path_wkthmltopdf = r'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe'
     else:
-        path_wkthmltopdf = '/app/storage/wkhtmltopdf'
+        path_wkthmltopdf = '/app/storage/wkhtmltopdf/wkhtmltopdf'
 
     config = pdfkit.configuration(wkhtmltopdf=path_wkthmltopdf)
 
