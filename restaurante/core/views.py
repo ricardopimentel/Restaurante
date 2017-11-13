dca79516166c691ba61be17e33ccfa73e46af02f
ricardopimentel <ricardoppimentel@gmail.com>
2017-11-13 19:33:28 -0300

    404 e 500 templates

--- restaurante/core/views.py
+++ restaurante/core/views.py
@@ -1,9 +1,15 @@
-from django.shortcuts import render, redirect, resolve_url as r
+from django.http import Http404
+from django.shortcuts import render, redirect, resolve_url as r, render_to_response
+
 
 # Create your views here.
+from django.template import RequestContext
+
+
 def Home(request):
     try:# Verificar se usuario esta logado
         if request.session['nome']:
             return render(request, 'index.html', {'err': '', 'itemselec': 'HOME'})
+
     except KeyError:
         return redirect(r('Login'))
