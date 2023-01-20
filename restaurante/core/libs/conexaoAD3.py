d27f6f3511288a45c5a8400f7b4e2ba7fdd2fdf3
unknown <ricardoppimentel@gmail.com>
2023-01-20 13:55:20 -0300

    Corrigindo lib de conexao com Ad, para adequar aos novos parametros do SUAP

--- restaurante/core/libs/conexaoAD3.py
+++ restaurante/core/libs/conexaoAD3.py
@@ -63,7 +63,7 @@ class conexaoAD(object):
                             read_only=True,
                             check_names=True,
                             user=self.LDAP_USERNAME, password=self.password) as c:
-                user_filter = '(&(!(userAccountControl:1.2.840.113556.1.4.803:=2))(memberof=CN=G_PARAISO_DO_TOCANTINS_ALUNOS, CN=Users,DC=ifto,DC=local))'
+                user_filter = '(&(!(userAccountControl:1.2.840.113556.1.4.803:=2))(memberof=CN=G_CA-PARAISO_ALUNOS, OU=Alunos, OU=CA-PARAISO, OU=IFTO,DC=ifto,DC=local))'
                 c.search(search_base='OU=Alunos, OU=CA-PARAISO, OU=IFTO, '+ self.base, search_filter=user_filter, search_scope=SUBTREE,
                          attributes=['description', 'mail', 'sAMAccountName', 'displayName'],
                          get_operational_attributes=False)
